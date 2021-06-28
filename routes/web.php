4055716acbff52b5fb5eba038f06eba6883e1d94
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-06-28 11:24:07 +0200

    Move to TALL stack

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PredictionController;
+use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\Auth\Passwords\Confirm;
+use App\Http\Livewire\Auth\Passwords\Email;
+use App\Http\Livewire\Auth\Passwords\Reset;
+use App\Http\Livewire\Auth\Register;
+use App\Http\Livewire\Auth\Verify;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +21,36 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-Route::patch('/predictions', [PredictionController::class, 'update'])->middleware(['auth', 'verified'])->name('predictions.update');
+Route::view('/', 'welcome')->name('home');
 
-require __DIR__.'/auth.php';
+Route::middleware('guest')->group(function () {
+    Route::get('login', Login::class)
+        ->name('login');
+
+    Route::get('register', Register::class)
+        ->name('register');
+});
+
+Route::get('password/reset', Email::class)
+    ->name('password.request');
+
+Route::get('password/reset/{token}', Reset::class)
+    ->name('password.reset');
+
+Route::middleware('auth')->group(function () {
+    Route::get('email/verify', Verify::class)
+        ->middleware('throttle:6,1')
+        ->name('verification.notice');
+
+    Route::get('password/confirm', Confirm::class)
+        ->name('password.confirm');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('email/verify/{id}/{hash}', EmailVerificationController::class)
+        ->middleware('signed')
+        ->name('verification.verify');
+
+    Route::post('logout', LogoutController::class)
+        ->name('logout');
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,27 +1,23 @@
 const mix = require("laravel-mix");
 
+require("laravel-mix-tailwind");
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
-    .vue({ version: 3 })
-    .postCss("resources/css/app.css", "public/css", [
-        require("postcss-import"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-    ])
-    .version()
-    .sourceMaps()
-    .webpackConfig(require("./webpack.config"))
-    .browserSync("localhost", {open: false});
+mix.js("resources/js/app.js", "public/js/app.js")
+    .sass("resources/sass/app.scss", "public/css/app.css")
+    .tailwind("./tailwind.config.js")
+    .browserSync("localhost", {open: false})
+    .sourceMaps();
 
 if (mix.inProduction()) {
     mix.version();
